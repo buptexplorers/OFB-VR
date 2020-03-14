5d119c0814e867d99d4dc647d9b6e06023fd854e
buptexplorers <839794840@qq.com>
2020-03-14 11:36:41 +0800

    Update and rename calcTileMseFlowForPlato.m to calcTileMseFlowForRL.m

--- calcTileMseFlowForRL.m
+++ calcTileMseFlowForRL.m
@@ -1,27 +1,24 @@
 function [viewedTiles,PMSE] = ...
-    calcTileMseFlowForPlato(set, vid,sec,frame,tiling,tileW,tileH,transRealOrPred,calcMSERealOrPred,only42and22)
-% transRealOrPred表示传输用什么视点，唯一的作用是确定传哪些块，calcMSERealOrPred表示算MSE用什么
-% only42and22为0则只计算QP=22和42两档
+    calcTileMseFlowForRL(set, vid,sec,frame,tiling,tileW,tileH,transRealOrPred,calcMSERealOrPred,only42and22)
+% input: original video, a sequence of frames, user's viewpoint at each frame
+%        transRealOrPred indicates transmitting real viewpoint data or prediected data, which is related to certain tiles
+%        calcMSERealOrPred indicates calculating MSE with real viewpoint data or prediected data
+%        only42and22 indicates the QP level
+% output: viewedTiles indicates which tile will be seen by users, MSE are error between tiles
 if only42and22==0
     QPrange = [22,26,30,34,38,42];
 else
     QPrange = [22,42];
 end
-% input: original video, a sequence of frames, user's viewpoint at each frame
-usernum = 10;
-frameBase = 0;
+
+usernum = 10; % important parameter, suggested range 10-48; a larger usernum leads to a more accurate calculation result
+frameBase = 0; % important parameter, change it according to the optical flow files (eg.20 if OF files generate from 20th second)
 frameGap = 15;
 
-frameAbs = (sec-1)*30 + frame;
+frameAbs = (sec-1)*30 + frame; % accurate frame number of a frame in a certain second
 
 orgChunkPath = sprintf('videos/%d/%d/%03d.mp4',set,vid,sec-1);
-%orgVideoPath = sprintf('videos/%d/%03d.mp4',set,vid);
-%orgChunkPath = sprintf('videos/%d/%03d/%03d.mp4',set,vid,sec);
-% if ~exist(orgChunkPath,'file')
-%     mkdir(sprintf('videos/%d/%03d',set,vid));
-%     command=['ffmpeg -y -ss ',num2str(sec-1),' -t 1 -r 30 -i ',orgVideoPath,' -r 30 -c:v libx264 -preset medium -c:a copy ',orgChunkPath];
-%     system(command);
-% end
+
 vr = VideoReader(orgChunkPath);
 
 % JND fitting function from user study
@@ -32,49 +29,29 @@ load('fitted.mat');
 % nine frame before this frame are also needed for the luminance calculation.
 orgFrameImgFolderPath = sprintf('image/%d/%03d',set,vid);
 mkdir(orgFrameImgFolderPath);
-%lastSecVr = -1;
+
 for f=frameAbs-29:frameAbs
     if ~exist(sprintf('%s/%04d_org.png', orgFrameImgFolderPath, f),'file')
-        %the frame may be at the last second.
-%          try
+        % the frame may be at the last second.
         if f <= (sec-1)*30
-           % if lastSecVr == -1
-%                 if ~exist(sprintf('videos/%d/%03d/%03d.mp4',set,vid,sec-1))
-%                     command=['ffmpeg -y -ss ',num2str(sec-2),' -t 1 -r 30 -i ',orgVideoPath,' -r 30 -c:v libx264 -preset superfast -c:a copy ',sprintf('videos/%d/%03d/%03d.mp4',set,vid,sec-1)];
-%                     system(command);
-%                 end
-%                 lastSecVr = VideoReader(sprintf('videos/%d/%03d/%03d.mp4',set,vid,sec-1));
-         %       lastSecVr = VideoReader(sprintf('videos/%d/%d/%03d.mp4',set,vid,sec-2));
-           % end
-         %   tempImg = read(lastSecVr, f - (sec-2)*30);
             command = ['ffmpeg -i ',sprintf('videos/%d/%d/%03d.mp4',set,vid,sec-2),' -vf "select=eq(n\,',num2str(f - (sec-2)*30-1),')" -vframes 1 ',sprintf('%s/%04d_org.png', orgFrameImgFolderPath, f)];
             system(command);
-            %tempImg = imread(sprintf('%s/%04d_org.png', orgFrameImgFolderPath, f));
         else
-%             vr = VideoReader(orgChunkPath);
-%             tempImg = read(vr, f - (sec-1)*30);
             command = ['ffmpeg -i ',sprintf('videos/%d/%d/%03d.mp4',set,vid,sec-1),' -vf "select=eq(n\,',num2str(f - (sec-1)*30-1),')" -vframes 1 ',sprintf('%s/%04d_org.png', orgFrameImgFolderPath, f)];
             system(command);
-            %tempImg = imread(sprintf('%s/%04d_org.png', orgFrameImgFolderPath, f));
         end
-%          catch
-%              1
-%          end
-        % imwrite(orgFrameImg, orgFrameImgPath);
-        %save(sprintf('%s/%04d_org.mat',orgFrameImgFolderPath, f),'tempImg');
     end
 end
 
 orgFrameImg = imread(sprintf('%s/%04d_org.png', orgFrameImgFolderPath, frameAbs));
 orgFrameImg = rgb2gray(orgFrameImg);
 
-
 % make sure the qpVideo exists and get frame images with different qp
 qpFrameImgs = {};
 for qp=QPrange
     try
         test = qpFrameImgs{qp - 22 + 1};
-    catch % 表示qpFrameImgs{qp - 22 + 1}不存在
+    catch % when qpFrameImgs{qp - 22 + 1} do not exist, generate it with ffmpeg
         qpChunkPath = sprintf('videos/%d/%d/%03d_%02d.mp4',set,vid,sec,qp);
         qpFrameImgPath = sprintf('image/%d/%03d/%03d/%02d_%02d.png',set,vid,sec,frame,qp);
         if ~exist(qpFrameImgPath, 'file')
@@ -83,8 +60,6 @@ for qp=QPrange
                 command = sprintf('ffmpeg -r 30 -i %s -r 30 -c:v libx264 -qp %d %s', orgChunkPath, qp, qpChunkPath);
                 system(command);
             end
-%             qpFrameImg = read(VideoReader(qpChunkPath), frame);
-%             save(qpFrameImgPath,'qpFrameImg');
             command = ['ffmpeg -i ',qpChunkPath,' -vf "select=eq(n\,',num2str(frame-1),')" -vframes 1 ',qpFrameImgPath];
             system(command);
         end
@@ -93,14 +68,12 @@ for qp=QPrange
     end
 end
 
-%disp('chunks & images prepared.');
-
-%% 0 R - the JND of static image (calculate if it doesn't exist)
+%% 1 the JND of static image (calculate if it doesn't exist)
 %TODO ~exist
 if exist(sprintf('SJND/%d/%03d/%04d.mat',set,vid,frameAbs),'file')
     SJND = cell2mat(struct2cell(load(sprintf('SJND/%d/%03d/%04d.mat',set,vid,frameAbs))));
 else
-    SJND = CalSJND_FAST_GPU2(double(orgFrameImg));
+    SJND = GetSJND(double(orgFrameImg));
     mkdir(sprintf('SJND/%d/%03d',set,vid));
     save(sprintf('SJND/%d/%03d/%04d.mat',set,vid,frameAbs),'SJND');
 end
@@ -108,7 +81,7 @@ end
 MSE = zeros(usernum,size(tiling,1),42-22+1); % nUser * nubmer of tiles * (42 - 22 + 1)
 viewedTiles = zeros(usernum,size(tiling,1)); % nUser * nubmer of tiles
 
-for user=1:usernum%%%DEBUG
+for user=1:usernum
     disp(['user ', num2str(user)]);
     %% 2 Fresult_all - a factor referring to distance from viewpoint
     H = vr.Height;
@@ -116,9 +89,7 @@ for user=1:usernum%%%DEBUG
     transFresult_all=ones(H,W)*10;
     calcMSEFresult_all=ones(H,W)*10;
     
-    %viewpoint = cell2mat(struct2cell(load(sprintf('viewpoint/%03d/%04d.mat',vid,frameAbs))));
     realViewpoint = load(['viewpoint/','real','/',num2str(set),'/',num2str(user),'_',num2str(vid-1),'.txt']);
-    % Center=[vertical index, horizontal index]
     if strcmp(transRealOrPred,'real')==1
         transCenter = [floor(H * realViewpoint(frameAbs,3)), floor(W * realViewpoint(frameAbs,2))];
     else
@@ -126,7 +97,6 @@ for user=1:usernum%%%DEBUG
     end
     transCenter = max(transCenter, [1,1]);
     
-    % Center=[vertical index, horizontal index]
     if strcmp(calcMSERealOrPred,'real')==1
         calcMSECenter = [floor(H * realViewpoint(frameAbs,3)), floor(W * realViewpoint(frameAbs,2))];
     else
@@ -134,17 +104,16 @@ for user=1:usernum%%%DEBUG
     end
     calcMSECenter = max(calcMSECenter, [1,1]);
     
-    % all user's viewpoint at this frame. ? * 2
-    
-    % 固定视口范围为840*630
+    % all user's viewpoint at this frame.
+    % a static viewport with the size of 840*630
     for j=transCenter(1)-314:transCenter(1)+315
         for k=transCenter(2)-419:transCenter(2)+420
             x=[j,k];
-            d=sqrt((x(1)-transCenter(1))^2+(x(2)-transCenter(2))^2);%到参照中心的距离
-            N=840;% 视口宽度
-            v=0.43; %圆区域
-            e=(atan(d/(N*v))/pi)*180;%圆形方程 半径N*v/2
-            transFresult_temp=0.06*max(0,(e-30))+1;%0.06*max(0,(e-30))+1;
+            d=sqrt((x(1)-transCenter(1))^2+(x(2)-transCenter(2))^2); % distance between a pixel and the viewpoint(center of viewport)
+            N=840; % width of viewport
+            v=0.43; % a circle area
+            e=(atan(d/(N*v))/pi)*180; % formula of the circle area, radius=N*v/2
+            transFresult_temp=0.06*max(0,(e-30))+1;
             X=j;
             Y=k;
             if j<=0
@@ -160,35 +129,36 @@ for user=1:usernum%%%DEBUG
                 Y=(k-W);
             end
             transFresult_all(X,Y)= transFresult_temp;
-            calcMSEFresult_all(X,Y)= transFresult_temp;%louis
+            calcMSEFresult_all(X,Y)= transFresult_temp;
         end
     end
     
     Ttrans=zeros(H,W);
-    % 表示传的区域
+    % store data of the area that will be transmitted
     Ttrans(transFresult_all<9) = 1;
     
     TcalcMSE=zeros(H,W);
     TcalcMSE(calcMSEFresult_all<9) = 1;
-    %% 4 DepthJND(front/ground):1~4
+	
+    %% 3 DepthJND(front/ground) by optical flow
     DepthPath = 'orgFlow/';
     floAbs = frameAbs-frameBase*30;
     DepthPath = strcat(DepthPath,int2str(set),'/',int2str(vid-1),'/','continuesFlo/');
-    load([DepthPath,'../orgFlo.mat']);
+    load([DepthPath,'../orgFlo.mat']); % get basic optical flow data
     for i = 1:6-length(int2str(floAbs))
         DepthPath = strcat(DepthPath,'0');
     end
-    DepthPath = strcat(DepthPath,int2str(floAbs-1),'.flo');
+    DepthPath = strcat(DepthPath,int2str(floAbs-1),'.flo'); % the floAbs of .flo file start from 0
     
     if ~exist([DepthPath,'.mat'],'file')
-        %读取原始flo
+        % read original .flo files
         orgFlo = 2*readFlowFile(DepthPath);
-        %填充至720*1440
+        % set the size of optical flow files with 720*1440
         Flo = zeros(H/2,W/2,2);
         flowSize = size(orgFlo);
         margin = [(H/2-flowSize(1))/2,(W/2-flowSize(2))/2];
         Flo(margin(1)+1:H/2-margin(1),margin(2)+1:W/2-margin(2),:) = orgFlo;
-        %分块求均值，下采样至12*24
+        % calculate average number according to tiles, downsampling to the size of 12*24
         floU = im2col(Flo(:,:,1),[tileH/2 tileW/2],'distinct');
         floV = im2col(Flo(:,:,2),[tileH/2 tileW/2],'distinct');
         tileFlo = reshape( [mean(floU),mean(floV)],12, 24, 2);
@@ -196,8 +166,8 @@ for user=1:usernum%%%DEBUG
     else
         load([DepthPath,'.mat']);
     end
-    DepthFlo = sum(abs(tileFlo).^2,3).^(1/2);%逐位求模
-    DepthFlo = DepthFlo/max(max(DepthFlo));%归一化
+    DepthFlo = sum(abs(tileFlo).^2,3).^(1/2); % calculate modulo of each element in the martix
+    DepthFlo = DepthFlo/max(max(DepthFlo)); % normalization
     
     centTile = [floor(transCenter(1)/tileH), floor(transCenter(2)/tileW)];
     for i = 1:H/tileH
@@ -208,23 +178,20 @@ for user=1:usernum%%%DEBUG
         end
     end
     
-    %上采样
+    % upsampling
     DepthJND = zeros(H,W);
     for i = 1:120
         for j = 1:120
             DepthJND(i:120:end,j:120:end) = DepthFlo;
         end
     end
-    %
     referenceD = DepthJND(transCenter(1),transCenter(2));
-    DepthJND = 1+3*abs(DepthJND - referenceD); %将DepthJND限制在1~4范围内，与Pano对标
+    DepthJND = 1+3*abs(DepthJND - referenceD); % normalize DepthJND within 1~4 to compare with Pano scheme
     
-    
+    %% 5 SpeedJND by optical flow
     'speed'
-    %% 5 SpeedJND
-    % 计算参考帧前15帧的光流信息
-    %
-    %
+    % calculate the optical flow information of 15 frames before the reference frame
+    % consider the rotation direction and speed of a user 
     RSpeedPath = sprintf('RotationVector/%s/%d/%03d/%02d.mat',calcMSERealOrPred,set,vid,user);
     if ~exist(RSpeedPath,'file')
         RotationVector=GetRotationVector(vid,user,15);
@@ -242,7 +209,7 @@ for user=1:usernum%%%DEBUG
     else
         RotationSpeed=cell2mat(struct2cell(load(RSpeedPath)));
     end
-    if strcmp(calcMSERealOrPred,'real')==1 % 等于real
+    if strcmp(calcMSERealOrPred,'real')==1 % when calculating real data
         speedLowerBound = min(RotationSpeed((sec*2+round(frame/15)-4):(sec*2+round(frame/15)-1)));
     else
         speedLowerBound = min(RotationSpeed((sec*2+round(frame/15)-4-2):(sec*2+round(frame/15)-1-2)));
@@ -258,10 +225,10 @@ for user=1:usernum%%%DEBUG
         for j = 1:6-length(int2str(floAbs))
             speedFloPath = strcat(speedFloPath,'0');
         end
-        speedFloPath = strcat(speedFloPath,int2str(floAbs-1),'.flo');
+        speedFloPath = strcat(speedFloPath,int2str(floAbs-1),'.flo'); % the floAbs of .flo file start from 0
         load([speedFloPath,'.mat']);
-        %计入总运动向量中
-        speedFlo = speedFlo + tileFlo/1440*2*pi;%换为度数角速度
+        % add into the total motion vector
+        speedFlo = speedFlo + tileFlo/1440*2*pi; % transform into number of degrees
     end
     
     for j=calcMSECenter(1)-314:calcMSECenter(1)+315
@@ -292,7 +259,7 @@ for user=1:usernum%%%DEBUG
         end
     end
     
-    %%
+    %% R - JND matrix of a certain frame
     % TODO
     % SJND 3-28
     % depth 1-4
@@ -302,10 +269,10 @@ for user=1:usernum%%%DEBUG
     %DEBUG
     R=R * 0.5;
     
-    %%
+    %% Get result of tiles
+    'tiles'
     % input: video with different qp, source video, JND matrix (R)
     % output: PMSE of each tile which is viewed
-    'tiles'
     for i=1:size(tiling,1)
         sr = tiling(i,1);
         er = tiling(i,2);
@@ -315,43 +282,54 @@ for user=1:usernum%%%DEBUG
         erP = tileW * er;
         scP = tileH * (sc-1) + 1;
         ecP = tileH * ec;
-        % 不能直接用tile位置，需计算tile与T的交集
-        % 纵向
+        % find out the viewed tiles
+        % vertical
         vtcTile = zeros(1,H);
         vtcTile(srP:erP)=1;
         vtcIntscTrans = max(Ttrans')==1 & vtcTile==1;
         vtcIntscCalcMSE = max(TcalcMSE')==1 & vtcTile==1;
-        % 横向
+        % horizontal
         hrzTile = zeros(1,W);
         hrzTile(scP:ecP)=1;
         hrzIntscTrans = max(Ttrans)==1 & hrzTile==1;
         hrzIntscCalcMSE = max(TcalcMSE)==1 & hrzTile==1;
         if sum(vtcIntscTrans(:))*sum(hrzIntscTrans(:))>0
-            %会传
-            %viewedTilesIndex = viewedTilesIndex + 1;
             viewedTiles(user,i) = 1;
         end
-        if sum(vtcIntscCalcMSE(:))*sum(hrzIntscCalcMSE(:))==0 % 如果和calcMSEviewport没有交集，不管传不传都是零
+        if sum(vtcIntscCalcMSE(:))*sum(hrzIntscCalcMSE(:))==0 % set MSE=0 if the tiles not being viewed by users
             MSE(user,i,:) = zeros(1,42-22+1);
-        else
-            
-            oneTileMSE = zeros(1,42-22+1);
-            
+        else            
+            oneTileMSE = zeros(1,42-22+1);            
             if sum(vtcIntscTrans(:))*sum(hrzIntscTrans(:))>0
                 for qp = QPrange
-                    % calc PMSE
-                    %oneTileMSE(qp-22+1)=CalPMSEPerTileGra(Center,orgFrameImg(scP:ecP,srP:erP),qpFrameImgs{qp-22+1}(scP:ecP,srP:erP),R(scP:ecP,srP:erP),T(scP:ecP,srP:erP));
+                    % calculate within a specific image but not a whole one, so the data is downsampled
+                    img_raw=orgFrameImg(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                    img_target=qpFrameImgs{qp-22+1}(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                    R0=R(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                    T=TcalcMSE(vtcIntscCalcMSE,hrzIntscCalcMSE);
                     
-                    oneTileMSE(qp-22+1)=CalPMSEPerTileGra(orgFrameImg(vtcIntscCalcMSE,hrzIntscCalcMSE),qpFrameImgs{qp-22+1}(vtcIntscCalcMSE,hrzIntscCalcMSE),R(vtcIntscCalcMSE,hrzIntscCalcMSE),TcalcMSE(vtcIntscCalcMSE,hrzIntscCalcMSE));
+                    D_JND=double(abs(double(img_target)-double(img_raw)));
+                    C=D_JND-R0;
+                    temp=(C>0).*(T==1).*(C.^2);
+                    
+                    oneTileMSE(qp-22+1)=sum(temp(:));
                 end
             else
-                oneTileMSE(42-22+1)=CalPMSEPerTileGra(orgFrameImg(vtcIntscCalcMSE,hrzIntscCalcMSE),qpFrameImgs{42-22+1}(vtcIntscCalcMSE,hrzIntscCalcMSE),R(vtcIntscCalcMSE,hrzIntscCalcMSE),TcalcMSE(vtcIntscCalcMSE,hrzIntscCalcMSE));
+                img_raw=orgFrameImg(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                img_target=qpFrameImgs{42-22+1}(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                R0=R(vtcIntscCalcMSE,hrzIntscCalcMSE);
+                T=TcalcMSE(vtcIntscCalcMSE,hrzIntscCalcMSE);
                 
-                %BandWidth_static(k,qp)=Function2{i,j}(qp);
+                D_JND=double(abs(double(img_target)-double(img_raw)));
+                C=D_JND-R0;
+                temp=(C>0).*(T==1).*(C.^2);
+                
+                oneTileMSE(42-22+1)=sum(temp(:));
             end
             MSE(user,i,:) = oneTileMSE;
         end
     end
+    % fetch MSE value of 7 specific QP level
     PMSE=zeros(usernum,size(tiling,1),7);
     for i=22:4:42
         level=i-22+1;
@@ -359,7 +337,6 @@ for user=1:usernum%%%DEBUG
         PMSE(:,:,k)=MSE(:,:,level);
     end
 end
-
 
 disp(['MSE set ',num2str(set), ' vid ',num2str(vid), ' sec ',num2str(sec), ' frame ',num2str(frame)]);
 
