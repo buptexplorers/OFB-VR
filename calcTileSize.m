95f19755862146fc5456e8137c2dee1eea88b1ea
buptexplorers <839794840@qq.com>
2020-03-14 11:41:41 +0800

    Update calcTileSize.m

--- calcTileSize.m
+++ calcTileSize.m
@@ -1,19 +1,16 @@
+function [tileSize] = calcTileSize(set,vid,sec,sr,er,sc,ec,tileH,tileW,qp)
 % input: video, tiling infomation
 % output: bitrate of videos with different qp
-function [tileSize] = calcTileSize(set, vid,sec,sr,er,sc,ec,tileH,tileW,qp) %TODO 调整其他程序
 
 chunkPath = sprintf('videos/%d/%d/%03d.mp4',set,vid,sec-1);
-%chunkPath = sprintf('videos/%d/%03d/%03d.mp4',set,vid,sec);
 
-%tileSizePath = sprintf('..\tileSize\%03d\%03d.mat',vid,sec);
-
-%get the pixel-scale boundary
+% get the pixel-scale boundary
 srP = tileH * (sr-1) + 1; % ffmpeg position start from 1
 scP = tileW * (sc-1) + 1;
 hP = tileH * (er-sr+1);
 wP = tileW * (ec-sc+1);
 tilePath = sprintf('tiles/%d/%03d/%03d/%04d_%04d_%04d_%04d_%04d.mp4',set,vid,sec,wP,hP,scP,srP,qp);
-%check if it exists
+% check if it exists
 if ~exist(tilePath,'file')
     mkdir(sprintf('tiles/%d/%03d/%03d',set,vid,sec));
     command = sprintf('ffmpeg -r 30 -i %s -r 30 -vf crop=%d:%d:%d:%d -c:v libx264 -qp %d %s', ...
@@ -29,5 +26,4 @@ fsize = fsize /1024;
 fsize = fsize*8; % Kbit
 tileSize = fsize;
 
-%save(tileSizePath,'tileSize');
 end
